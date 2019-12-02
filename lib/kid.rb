702412a97db6b638c9a4721a8eb234e753ebfce7
Winston Pemberton <winstonpemberton@gmail.com>
2019-12-02 23:42:25 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,9 +3,8 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
 class Kid
-
-  include FancyDance::InstanceMethods
   extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 
   attr_accessor :name
 
